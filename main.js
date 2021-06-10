45446d2f128dd23698f24aac4eb389cb004056c5
Ashokshanbhag <79842743+Ashokshanbhag@users.noreply.github.com>
2021-06-10 16:29:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function preload(){
+
+    
+
+}
+
+function setup(){
+
+    canvas = createCanvas(400,400);
+    canvas.center();
+
+}
+
+function draw(){
+
+
+
+}
+
+function takeSnapshot(){
+
+    save('clownose of random person but most likely me.png');
+
+}
